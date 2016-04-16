135e3e4e63a564d18106d64d854914c9abcfd673
Kapiainen <kapiainen@users.noreply.github.com>
2016-04-16 13:58:24 +0300

    Updated build systems and project file

--- BuildPackages.py
+++ BuildPackages.py
@@ -1,27 +1,33 @@
 # Developed for Python 3.x
-import os, zipfile
+import os, zipfile, sys
+
+ROOT = ""
+if len(sys.argv) > 1:
+	ROOT = sys.argv[1]
+else:
+	ROOT = os.getcwd()
 
 PACKGE_PREFIX = "SublimePapyrus"
 PACKAGE_EXTENSION = ".sublime-package"
 
 def main():
-	cwd = os.getcwd()
-	licenseFile = os.path.join(cwd, "LICENSE.md")
+	global ROOT
+	licenseFile = os.path.join(ROOT, "LICENSE.md")
 	if not os.path.isfile(licenseFile):
-		print("Could not find 'LICENSE.md' in '%s'." % cwd)
+		print("Could not find 'LICENSE.md' in '%s'." % ROOT)
 		return
 	licenseRelPath = os.path.relpath(licenseFile, os.path.split(licenseFile)[0])
-	readmeFile = os.path.join(cwd, "README.md")
+	readmeFile = os.path.join(ROOT, "README.md")
 	if not os.path.isfile(readmeFile):
-		print("Could not find 'README.md' in '%s'." % cwd)
+		print("Could not find 'README.md' in '%s'." % ROOT)
 		return
 	readmeRelPath = os.path.relpath(readmeFile, os.path.split(readmeFile)[0])
-	pkg = os.path.join(cwd, "Packages")
+	pkg = os.path.join(ROOT, "Packages")
 	if not os.path.isdir(pkg):
 		os.makedirs(pkg)
-	src = os.path.join(cwd, "Source")
+	src = os.path.join(ROOT, "Source")
 	if not os.path.isdir(src):
-		print("There is no 'Source' folder in '%s'." % cwd)
+		print("There is no 'Source' folder in '%s'." % ROOT)
 		return
 	core = os.path.join(src, "Core")
 	if os.path.isdir(core):
