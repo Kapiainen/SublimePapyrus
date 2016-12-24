44fd0188b016bfc643a4378509f1db58fb549dba
Kapiainen <kapiainen@users.noreply.github.com>
2016-12-24 18:36:47 +0200

    Updated syntactic analysis

--- Source/Modules/Fallout 4/SyntacticAnalysis.py
+++ Source/Modules/Fallout 4/SyntacticAnalysis.py
@@ -115,12 +115,12 @@ NodeDescription = [
 
 class Identifier(object):
 	__slots__ = [
-		"namespace", # list of string
-		"name", # string
+		"namespace", # list of str
+		"name", # str
 	]
 
 	def __init__(self, aIdentifier):
-		assert(isinstance(aIdentifier, list)) #Prune
+		assert isinstance(aIdentifier, list #Prune
 		self.name = aIdentifier.pop()
 		self.namespace = aIdentifier
 
@@ -132,9 +132,9 @@ class Type(object):
 	]
 
 	def __init__(self, aIdentifier, aArray, aStruct):
-		assert(isinstance(aIdentifier, Identifier)) #Prune
-		assert(isinstance(aArray, bool)) #Prune
-		assert(isinstance(aStruct, bool)) #Prune
+		assert isinstance(aIdentifier, Identifier) #Prune
+		assert isinstance(aArray, bool) #Prune
+		assert isinstance(aStruct, bool) #Prune
 		self.identifier = aIdentifier
 		self.isArray = aArray
 		self.isStruct = aStruct
@@ -152,8 +152,8 @@ class ArrayAtomNode(Node):
 	]
 
 	def __init__(self, aChild, aExpression):
-		assert(isinstance(aChild, Node)) #Prune
-		assert(isinstance(aExpression, Node)) #Prune
+		assert isinstance(aChild, Node) #Prune
+		assert isinstance(aExpression, Node) #Prune
 		self.child = aChild
 		self.expression = aExpression
 
@@ -172,8 +172,8 @@ class ArrayFuncOrIdNode(Node):
 	]
 
 	def __init__(self, aChild, aExpression):
-		assert(isinstance(aChild, Node)) #Prune
-		assert(isinstance(aExpression, Node)) #Prune
+		assert isinstance(aChild, Node) #Prune
+		assert isinstance(aExpression, Node) #Prune
 		self.child = aChild
 		self.expression = aExpression
 
@@ -185,9 +185,9 @@ class BinaryOperatorNode(Node):
 	]
 
 	def __init__(self, aOperator, aLeftOperand, aRightOperand):
-		assert(isinstance(aOperator, Token)) #Prune
-		assert(isinstance(aLeftOperand, Node)) #Prune
-		assert(isinstance(aRightOperand, Node)) #Prune
+		assert isinstance(aOperator, Token) #Prune
+		assert isinstance(aLeftOperand, Node) #Prune
+		assert isinstance(aRightOperand, Node) #Prune
 		self.operator = aOperator
 		self.leftOperand = aLeftOperand
 		self.rightOperand = aRightOperand
@@ -198,7 +198,7 @@ class ConstantNode(Node):
 	]
 
 	def __init__(self, aValue):
-		assert(isinstance(aValue, Token)) #Prune
+		assert isinstance(aValue, Token) #Prune
 		self.value = aValue
 
 class ExpressionNode(Node):
@@ -207,7 +207,7 @@ class ExpressionNode(Node):
 	]
 
 	def __init__(self, aChild):
-		assert(isinstance(aChild, Node)) #Prune
+		assert isinstance(aChild, Node) #Prune
 		self.child = aChild
 
 class FunctionCallArgumentNode(Node):
@@ -218,8 +218,8 @@ class FunctionCallArgumentNode(Node):
 
 	def __init__(self, aIdentifier, aValue):
 		if aIdentifier: #Prune
-			assert(isinstance(aIdentifier, Identifier)) #Prune
-		assert(isinstance(aValue, Expression)) #Prune
+			assert isinstance(aIdentifier, Identifier) #Prune
+		assert isinstance(aValue, Expression) #Prune
 		self.identifier = aIdentifier
 		self.value = aValue
 
@@ -230,8 +230,8 @@ class FunctionCallNode(Node):
 	]
 
 	def __init__(self, aIdentifier, aArguments):
-		assert(isinstance(aIdentifier, Identifier)) #Prune
-		assert(isinstance(aArguments, list)) #Prune
+		assert isinstance(aIdentifier, Identifier) #Prune
+		assert isinstance(aArguments, list) #Prune
 		self.identifier = aIdentifier
 		self.arguments = aArguments
 
@@ -241,7 +241,7 @@ class IdentifierNode(Node):
 	]
 
 	def __init__(self, aIdentifier):
-		assert(isinstance(aIdentifier, Identifier)) #Prune
+		assert isinstance(aIdentifier, Identifier) #Prune
 		self.identifier = aIdentifier
 
 class LengthNode(Node):
@@ -256,7 +256,7 @@ class StructCreationNode(Node):
 	]
 
 	def __init__(self, aIdentifier):
-		assert(isinstance(aIdentifier, Identifier)) #Prune
+		assert isinstance(aIdentifier, Identifier) #Prune
 		self.identifier = aIdentifier
 
 class UnaryOperatorNode(Node):
@@ -266,8 +266,8 @@ class UnaryOperatorNode(Node):
 	]
 
 	def __init__(self, aOperator, aOperand):
-		assert(isinstance(aOperator, Token)) #Prune
-		assert(isinstance(aOperand, Node)) #Prune
+		assert isinstance(aOperator, Token) #Prune
+		assert isinstance(aOperand, Node) #Prune
 		self.operator = aOperator
 		self.operand = aOperand
 
@@ -278,7 +278,7 @@ class ExpressionStatement(object):
 	]
 
 	def __init__(self, aExpression):
-		assert(isinstance(aExpression, Expression)) #Prune
+		assert isinstance(aExpression, Expression) #Prune
 		self.expression = aExpression
 
 class FunctionParameter(object):
@@ -289,9 +289,9 @@ class FunctionParameter(object):
 	]
 
 	def __init__(self, aIdentifier, aType, aDefaultValue):
-		assert(isinstance(aIdentifier, Identifier)) #Prune
-		assert(isinstance(aType, Type)) #Prune
-		assert(isinstance(aDefaultValue, Expression)) #Prune
+		assert isinstance(aIdentifier, Identifier) #Prune
+		assert isinstance(aType, Type) #Prune
+		assert isinstance(aDefaultValue, Expression) #Prune
 		self.identifier = aIdentifier
 		self.type = aType
 		self.defaultValue = aDefaultValue
@@ -305,33 +305,443 @@ class FunctionFlags(object):
 	]
 
 	def __init__(self, aBetaOnly, aDebugOnly, aGlobal, aNative):
-		assert(isinstance(aBetaOnly, bool)) #Prune
-		assert(isinstance(aDebugOnly, bool)) #Prune
-		assert(isinstance(aGlobal, bool)) #Prune
-		assert(isinstance(aNative, bool)) #Prune
+		assert isinstance(aBetaOnly, bool) #Prune
+		assert isinstance(aDebugOnly, bool) #Prune
+		assert isinstance(aGlobal, bool) #Prune
+		assert isinstance(aNative, bool) #Prune
 		self.isBetaOnly = aBetaOnly
 		self.isDebugOnly = aDebugOnly
 		self.isGlobal = aGlobal
 		self.isNative = aNative
 
-class FunctionSignature(object):
+class FunctionSignatureStatement(object):
 	__slots__ = [
 		"identifier", # Identifier
 		"type", # Type
-		"parameters", # List of FunctionParameter
+		"parameters", # list of FunctionParameter
 		"flags", # FunctionFlags
 		"line" # int
 	]
 
 	def __init__(self, aIdentifier, aType, aParameters, aFlags, aLine):
-		assert(isinstance(aIdentifier, Identifier)) #Prune
-		assert(isinstance(aType, Type)) #Prune
-		assert(isinstance(aParameters, list)) #Prune
-		assert(isinstance(aFlags, FunctionFlags)) #Prune
-		assert(isinstance(aLine, int)) #Prune
+		assert isinstance(aIdentifier, Identifier) #Prune
+		assert isinstance(aType, Type) #Prune
+		assert isinstance(aParameters, list) #Prune
+		assert isinstance(aFlags, FunctionFlags) #Prune
+		assert isinstance(aLine, int) #Prune
 		self.identifier = aIdentifier
 		self.type = aType
 		self.parameters = aParameters
 		self.flags = aFlags
 		self.line = aLine
 
+class CustomEventStatement(object):
+	__slots__ = [
+		"identifier", # Identifier
+		"line" # int
+	]
+
+	def __init__(self, aIdentifier, aLine):
+		assert isinstance(aIdentifier, Identifier) #Prune
+		assert isinstance(aLine, int) #Prune
+		self.identifier = aIdentifier
+		self.line = aLine
+
+class DocstringStatement(object):
+	__slots__ = [
+		"value", # str
+		"line" # int
+	]
+
+	def __init__(self, aValue, aLine):
+		assert isinstance(aValue, str) #Prune
+		assert isinstance(aLine, int) #Prune
+		self.value = aValue
+		self.line = aLine
+
+class AssignmentStatement(object):
+	__slots__ = [
+		"operator", # Token
+		"leftExpression", # ExpressionNode
+		"rightExpression", # ExpressionNode
+		"line" # int
+	]
+
+	def __init__(self, aOperator, aLeftExpression, aRightExpression, aLine):
+		assert isinstance(aOperator, Token) #Prune
+		assert isinstance(aLeftExpression, ExpressionNode) #Prune
+		assert isinstance(aRightExpression, ExpressionNode) #Prune
+		assert isinstance(aLine, int) #Prune
+		self.operator = aOperator
+		self.leftExpression = aLeftExpression
+		self.rightExpression = aRightExpression
+		self.line = aLine
+
+class ElseStatement(object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self, aLine):
+		assert isinstance(aLine, int) #Prune
+		self.line = aLine
+
+class ElseIfStatement(object):
+	__slots__ = [
+		"expression", # ExpressionNode
+		"line" # int
+	]
+
+	def __init__(self, aExpression, aLine):
+		assert isinstance(aExpression, ExpressionNode) #Prune
+		assert isinstance(aLine, int) #Prune
+		self.expression = aExpression
+		self.line = aLine
+
+class EndEventStatement(object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self, aLine):
+		assert isinstance(aLine, int) #Prune
+		self.line = aLine
+
+class EndFunctionStatement(object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self, aLine):
+		assert isinstance(aLine, int) #Prune
+		self.line = aLine
+
+class EndGroupStatement(object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self, aLine):
+		assert isinstance(aLine, int) #Prune
+		self.line = aLine
+
+class EndIfStatement(object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self, aLine):
+		assert isinstance(aLine, int) #Prune
+		self.line = aLine
+
+class EndPropertyStatement(object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self, aLine):
+		assert isinstance(aLine, int) #Prune
+		self.line = aLine
+
+class EndStateStatement(object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self, aLine):
+		assert isinstance(aLine, int) #Prune
+		self.line = aLine
+
+class EndStructStatement(object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self, aLine):
+		assert isinstance(aLine, int) #Prune
+		self.line = aLine
+
+class EndWhileStatement(object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self, aLine):
+		assert isinstance(aLine, int) #Prune
+		self.line = aLine
+
+class EventFlags(object):
+	__slots__ = [
+		"isNative" # bool
+	]
+
+	def __init__(self, aNative):
+		assert isinstance(aNative, bool) #Prune
+		self.isNative = aNative
+
+class EventParameter(object):
+	__slots__ = [
+		"identifier", # Identifier
+		"type", # Type
+	]
+
+	def __init__(self, aIdentifier, aType):
+		assert isinstance(aIdentifier, Identifier) #Prune
+		assert isinstance(aType, Type) #Prune
+		self.identifier = aIdentifier
+		self.type = aType
+
+class EventSignatureStatement(object):
+	__slots__ = [
+		"identifier", # Identifier
+		"remote", # Identifier
+		"parameters", # list of EventParameter
+		"flags", # EventFlags
+		"line" # int
+	]
+
+	def __init__(self, aIdentifier, aRemote, aParameters, aFlags, aLine):
+		assert isinstance(aIdentifier, Identifier) #Prune
+		assert isinstance(aRemote, Identifier) #Prune
+		assert isinstance(aParameters, list) #Prune
+		assert isinstance(aFlags, EventFlags))
+		assert isinstance(aLine, int) #Prune
+		self.identifier = aIdentifier
+		self.remote = aRemote
+		self.parameters = aParameters
+		self.flags = aFlags
+		self.line = aLine
+
+class GroupFlags(object):
+	__slots__ = [
+		"isCollapsed", # bool
+		"isCollapsedOnBase", # bool
+		"isCollapsedOnRef" # bool
+	]
+
+	def __init__(self, aCollapsed, aCollapsedOnBase, aCollapsedOnRef):
+		assert isinstance(aCollapsed, bool) #Prune
+		assert isinstance(aCollapsedOnBase, bool) #Prune
+		assert isinstance(aCollapsedOnRef, bool) #Prune
+		self.isCollapsed = aCollapsed
+		self.isCollapsedOnBase = aCollapsedOnBase
+		self.isCollapsedOnRef = aCollapsedOnRef
+
+class GroupSignatureStatement(object):
+	__slots__ = [
+		"identifier", # Identifier
+		"flags", # GroupFlags
+		"line" # int
+	]
+
+	def __init__(self, aIdentifier, aFlags, aLine):
+		assert isinstance(aIdentifier, Identifier) #Prune
+		assert isinstance(aFlags, GroupFlags) #Prune
+		assert isinstance(aLine, int) #Prune
+		self.identifier = aIdentifier
+		self.flags = aFlags
+		self.line = aLine
+
+class IfStatement(object):
+	__slots__ = [
+		"expression", # ExpressionNode
+		"line" # int
+	]
+
+	def __init__(self, aExpression, aLine):
+		assert isinstance(aExpression, ExpressionNode) #Prune
+		assert isinstance(aLine, int) #Prune
+		self.expression = aExpression
+		self.line = aLine
+
+class ImportStatement(object):
+	__slots__ = [
+		"identifier", # Identifier
+		"line" # int
+	]
+
+	def __init__(self, aIdentifier, aLine):
+		assert isinstance(aIdentifier, Identifier) #Prune
+		assert isinstance(aLine, int) #Prune
+		self.identifier = aIdentifier
+		self.line = aLine
+
+class PropertyFlags(object):
+	__slots__ = [
+		"isAuto", # bool
+		"isAutoReadOnly", # bool
+		"isConditional", # bool
+		"isConst", # bool
+		"isHidden", # bool
+		"isMandatory" # bool
+	]
+
+	def __init__(self, aAuto, aAutoReadOnly, aConditional, aConst, aHidden, aMandatory):
+		assert isinstance(aAuto, bool) #Prune
+		assert isinstance(aAutoReadOnly, bool) #Prune
+		assert isinstance(aConditional, bool) #Prune
+		assert isinstance(aConst, bool) #Prune
+		assert isinstance(aHidden, bool) #Prune
+		assert isinstance(aMandatory, bool) #Prune
+		self.isAuto = aAuto
+		self.isAutoReadOnly = aAutoReadOnly
+		self.isConditional = aConditional
+		self.isConst = aConst
+		self.isHidden = aHidden
+		self.isMandatory = aMandatory
+
+class PropertySignatureStatement(object):
+	__slots__ = [
+		"identifier", # Identifier
+		"type", # Type
+		"defaultValue", # ExpressionNode
+		"line" # int
+	]
+
+	def __init__(self):
+		pass
+#RETURN
+class (object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self):
+		pass
+#SCRIPTSIGNATURE
+class (object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self):
+		pass
+#STATESIGNATURE
+class (object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self):
+		pass
+#STRUCTSIGNATURE
+class (object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self):
+		pass
+#VARIABLE
+class (object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self):
+		pass
+#WHILE
+class (object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self):
+		pass
+#	#Caprica extensions
+#BREAK
+class (object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self):
+		pass
+#CONTINUE
+class (object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self):
+		pass
+#SWITCH
+class (object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self):
+		pass
+#CASE
+class (object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self):
+		pass
+#DEFAULT
+class (object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self):
+		pass
+#ENDSWITCH
+class (object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self):
+		pass
+#FOR
+class (object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self):
+		pass
+#ENDFOR
+class (object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self):
+		pass
+#FOREACH
+class (object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self):
+		pass
+#ENDFOREACH
+class (object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self):
+		pass
+#DO
+class (object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self):
+		pass
+#LOOPWHILE
+class (object):
+	__slots__ = [
+		"line" # int
+	]
+
+	def __init__(self):
+		pass
