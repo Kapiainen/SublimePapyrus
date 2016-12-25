695b1674f6b3195ad68480b626bc0b9710d9869a
Kapiainen <kapiainen@users.noreply.github.com>
2016-12-25 11:14:10 +0200

    Updated semantic analysis

--- Source/Modules/Fallout 4/SemanticAnalysis.py
+++ Source/Modules/Fallout 4/SemanticAnalysis.py
@@ -1,7 +1,27 @@
-import os
+import os, sys
+PYTHON_VERSION = sys.version_info
+if PYTHON_VERSION[0] == 2:
+	import imp
+	root, module = os.path.split(os.getcwd())
 
-# PLATFORM_WINDOWS is used to determine how to look for scripts in the filesystem. Unix-based OSes often have case-sensitive filesystems.
-PLATFORM_WINDOWS = os.name == "nt"
+	lexicalModule = os.path.join(root, module, "LexicalAnalysis.py")
+	imp.load_source("LexicalAnalysis", lexicalModule)
+
+	syntacticModule = os.path.join(root, module, "SyntacticAnalysis.py")
+	imp.load_source("SyntacticAnalysis", Module)
+
+	from LexicalAnalysis import *
+	from SyntacticAnalysis import *
+
+	# Cleaning up
+	del root
+	del module
+	del coreModule
+	del lexicalModule
+	del syntacticModule
+elif PYTHON_VERSION[0] >= 3:
+	from .LexicalAnalysis import *
+	from .SyntacticAnalysis import *
 
 class FunctionObject(object):
 	__slots__ = [
