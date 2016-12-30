939ff89820f7f697cc9743ad92c7b8b3e52de99a
Kapiainen <kapiainen@users.noreply.github.com>
2016-12-30 15:10:03 +0200

    Updated plugin

--- Source/Modules/Fallout 4/Plugin.py
+++ Source/Modules/Fallout 4/Plugin.py
@@ -146,15 +146,15 @@ class SublimePapyrusFallout4CompileScriptCommand(sublime_plugin.WindowCommand):
 class EventListener(sublime_plugin.EventListener):
 	def __init__(self):
 		super(EventListener,self).__init__()
-		self.validScope = "source.papyrus.fallout4"
-		self.linterQueue = 0
-		self.linterErrors = {}
-		self.linterRunning = False
-		self.completionRunning = False
+		self.valid_scope = "source.papyrus.fallout4"
+		self.linter_queue = 0
+		self.linter_errors = {}
+		self.linter_running = False
+		self.completion_running = False
 
 	def IsValidScope(self, view):
-		if self.validScope:
-			return self.validScope in view.scope_name(0)
+		if self.valid_scope:
+			return self.valid_scope in view.scope_name(0)
 		return False
 
 	def on_post_save(self, view):
@@ -162,26 +162,26 @@ class EventListener(sublime_plugin.EventListener):
 			# Check if extension is .psc and in one of the import folders or the scripts folder
 			#	Yes -> Check if the completions for script types needs to be updated
 			print("\nSublimePapyrus - Fallout 4 - OnPostSave: %s" % view.file_name())
-			scriptContents = view.substr(sublime.Region(0, view.size()))
+			script_contents = view.substr(sublime.Region(0, view.size()))
 			try:
-				scriptName = Linter.GetScriptName(scriptContents)
+				script_name = Linter.GetScriptName(script_contents)
 			except LexicalAnalysis.LexicalError as e:
 				return
 			except SyntacticAnalysis.SyntacticError as e:
 				return
-			if scriptName:
-				Linter.ClearCache(":".join(scriptName).upper())
+			if script_name:
+				Linter.ClearCache(":".join(script_name).upper())
 			else:
 				return
 
 			return
-			region = view.find("^\s*scriptname\s+([_a-z0-9:]+)\s", 0, sublime.IGNORECASE)
+			region = view.find("^\s*scriptname_ns+([_a-z0-9:]+)\s", 0, sublime.IGNORECASE)
 			if region:
-				scriptName = view.substr(region).trim()
-				scriptName = scriptName[10:].trim()
-				print("ScriptName:", scriptName, scriptName.split(":"))
+				script_name = view.substr(region).trim()
+				script_name = script_name[10:].trim()
+				print("Script_name:", script_name, script_name.split(":"))
 			else:
-				print("Could not find ScriptName.")
+				print("Could not find Script_name.")
 
 	def on_modified(self, view):
 		if self.IsValidScope(view):
@@ -190,135 +190,100 @@ class EventListener(sublime_plugin.EventListener):
 			# Tooltips
 			global SUBLIME_VERSION
 			if SUBLIME_VERSION >= 3070 and settings.get("tooltip_function_parameters", True):
-				if self.linterRunning:
+				if self.linter_running:
 					return
-				elif self.completionRunning:
+				elif self.completion_running:
 					return
 			# Linter
 			if settings and settings.get("linter_on_modified", True):
 				self.QueueLinter(view)
 
 	def QueueLinter(self, view):
-		if self.linterRunning: # If an instance of the linter is running, then cancel
+		if self.linter_running: # If an instance of the linter is running, then cancel
 			return
-		self.linterQueue += 1 # Add to queue
+		self.linter_queue += 1 # Add to queue
 		settings = SublimePapyrus.GetSettings()
 		delay = 0.500
 		if settings:
 			delay = settings.get("linter_delay", 500)/1000.0
 			if delay < 0.050:
 				delay = 0.050
-		self.bufferID = view.buffer_id()
-		if self.bufferID:
+		self.buffer_id = view.buffer_id()
+		if self.buffer_id:
 			SublimePapyrus.ClearLinterHighlights(view)
 			modules = settings.get("modules", None)
 			if modules:
-				moduleSettings = modules.get("fallout4", None)
-				if moduleSettings:
-					sourcePaths = SublimePapyrus.GetSourcePaths(view)
-					#scripts = moduleSettings.get("scripts", None)
+				module_settings = modules.get("fallout4", None)
+				if module_settings:
+					source_paths = SublimePapyrus.GetSourcePaths(view)
+					#scripts = module_settings.get("scripts", None)
 					#if scripts:
-						#sourcePaths.insert(0, scripts)
-					lineNumber, columnNumber = view.rowcol(view.sel()[0].begin())
-					lineNumber += 1
-					scriptContents = view.substr(sublime.Region(0, view.size()))						
+						#source_paths.insert(0, scripts)
+					line_number, column_number = view.rowcol(view.sel()[0].begin())
+					line_number += 1
+					script_contents = view.substr(sublime.Region(0, view.size()))						
 					args = None
 					caprica = "fallout4.caprica" in view.scope_name(0)
 					if PYTHON_VERSION[0] == 2:
-						args = {"aView": None, "aLineNumber": lineNumber, "aSource": scriptContents, "aPaths": sourcePaths, "aCaprica": caprica}
+						args = {"a_view": None, "a_line_number": line_number, "a_source": script_contents, "a_paths": source_paths, "a_caprica": caprica}
 					elif PYTHON_VERSION[0] >= 3:
-						args = {"aView": view, "aLineNumber": lineNumber, "aSource": scriptContents, "aPaths": sourcePaths, "aCaprica": caprica}
+						args = {"a_view": view, "a_line_number": line_number, "a_source": script_contents, "a_paths": source_paths, "a_caprica": caprica}
 					if args:
 						t = threading.Timer(delay, self.RunLinter, kwargs=args)
 						t.daemon = True
 						t.start()
 
-	def RunLinter(self, aView, aLineNumber, aSource, aPaths, aCaprica):
-	# aView: A sublime.View object, possibly has the value None
-	# aLineNumber: Line that contains the cursor as an int
-	# aSource: Script to process as a string
-	# aPaths: List of paths to import folders
-	# aCaprica: True if Caprica extensions should be used, otherwise False
-		self.linterQueue -= 1 # Remove from queue
-		if self.linterQueue > 0: # If there is a queue, then cancel
+	def RunLinter(self, a_view, a_line_number, a_source, a_paths, a_caprica):
+	# a_view: A sublime.View object, possibly has the value None
+	# a_line_number: Line that contains the cursor as an int
+	# a_source: Script to process as a string
+	# a_paths: List of paths to import folders
+	# a_caprica: True if Caprica extensions should be used, otherwise False
+		self.linter_queue -= 1 # Remove from queue
+		if self.linter_queue > 0: # If there is a queue, then cancel
 			return
-		elif self.completionRunning: # If completions are being generated, then cancel
+		elif self.completion_running: # If completions are being generated, then cancel
 			return
-		self.linterRunning = True # Block further attempts to run the linter until this instance has finished
+		self.linter_running = True # Block further attempts to run the linter until this instance has finished
 		start = time.time() #DEBUG
 		def Run():
 			print("Running linter")
 			try:
-				Linter.Process(aSource, aPaths, aCaprica)
+				Linter.Process(a_source, a_paths, a_caprica)
 			except LexicalAnalysis.LexicalError as e:
 				print(e.message)
-				if aView:
-					SublimePapyrus.SetStatus(aView, "sublimepapyrus-linter", "Error on line %d, column %d: %s" % (e.line, e.column, e.message))
-					SublimePapyrus.HighlightLinter(aView, e.line, e.column)
+				if a_view:
+					SublimePapyrus.SetStatus(a_view, "sublimepapyrus-linter", "Error on line %d, column %d: %s" % (e.line, e.column, e.message))
+					SublimePapyrus.HighlightLinter(a_view, e.line, e.column)
 				return False
 			except SyntacticAnalysis.SyntacticError as e:
 				print(e.message)
-				if aView:
-					SublimePapyrus.SetStatus(aView, "sublimepapyrus-linter", "Error on line %d: %s" % (e.line, e.message))
-					SublimePapyrus.HighlightLinter(aView, e.line)
+				if a_view:
+					SublimePapyrus.SetStatus(a_view, "sublimepapyrus-linter", "Error on line %d: %s" % (e.line, e.message))
+					SublimePapyrus.HighlightLinter(a_view, e.line)
 				return False
 			except SemanticAnalysis.SemanticError as e:
 				print(e.message)
-				if aView:
-					SublimePapyrus.SetStatus(aView, "sublimepapyrus-linter", "Error on line %d: %s" % (e.line, e.message))
-					SublimePapyrus.HighlightLinter(aView, e.line)
+				if a_view:
+					SublimePapyrus.SetStatus(a_view, "sublimepapyrus-linter", "Error on line %d: %s" % (e.line, e.message))
+					SublimePapyrus.HighlightLinter(a_view, e.line)
 				return False
 			except SemanticAnalysis.RemoteError as e:
 				print(e.message)
-				if aView:
-					SublimePapyrus.SetStatus(aView, "sublimepapyrus-linter", e.message)
-					SublimePapyrus.HighlightLinter(aView, e.line)
+				if a_view:
+					SublimePapyrus.SetStatus(a_view, "sublimepapyrus-linter", e.message)
+					SublimePapyrus.HighlightLinter(a_view, e.line)
 				return False
 #			except Linter.MissingScript as e:
 #				print(e.message)
-#				if aView:
-#					SublimePapyrus.SetStatus(aView, "sublimepapyrus-linter", "Error on line %d: %s" % (e.line, e.message))
-#					SublimePapyrus.HighlightLinter(aView, e.line)
+#				if a_view:
+#					SublimePapyrus.SetStatus(a_view, "sublimepapyrus-linter", "Error on line %d: %s" % (e.line, e.message))
+#					SublimePapyrus.HighlightLinter(a_view, e.line)
 #				return False
 			return True
 
-#			global LEX
-#			global SYN
-#			global SEM
-#			try:
-#				script = SEM.Process(LEX, SYN, aSource, aPaths)
-#			except Linter.LexicalError as e:
-#				print(e.message)
-#				if aView:
-#					SublimePapyrus.SetStatus(aView, "sublimepapyrus-linter", "Error on line %d, column %d: %s" % (e.line, e.column, e.message))
-#					SublimePapyrus.HighlightLinter(aView, e.line, e.column)
-#				return False
-#			except Linter.SyntacticError as e:
-#				print(e.message)
-#				if aView:
-#					SublimePapyrus.SetStatus(aView, "sublimepapyrus-linter", "Error on line %d: %s" % (e.line, e.message))
-#					SublimePapyrus.HighlightLinter(aView, e.line)
-#				return False
-#			except Linter.SemanticError as e:
-#				print(e.message)
-#				if aView:
-#					SublimePapyrus.SetStatus(aView, "sublimepapyrus-linter", "Error on line %d: %s" % (e.line, e.message))
-#					SublimePapyrus.HighlightLinter(aView, e.line)
-#				return False
-#			except Linter.MissingScript as e:
-#				print(e.message)
-#				if aView:
-#					SublimePapyrus.SetStatus(aView, "sublimepapyrus-linter", "Error on line %d: %s" % (e.line, e.message))
-#					SublimePapyrus.HighlightLinter(aView, e.line)
-#				return False
-#			#except Exception as e:
-#			#	print("SublimePapyrus - Fallout 4 - %s" % e)
-#			#	if aView:
-#			#		SublimePapyrus.SetStatus(aView, "sublimepapyrus-linter", "Fallout 4 - %s" % e)
-#			return True
-
 		if Run():
-			if aView:
-				SublimePapyrus.ClearStatus(aView, "sublimepapyrus-linter")
+			if a_view:
+				SublimePapyrus.ClearStatus(a_view, "sublimepapyrus-linter")
 		print("Linter: Finished in %f milliseconds and releasing lock..." % ((time.time()-start)*1000.0)) #DEBUG
-		self.linterRunning = False
+		self.linter_running = False
